cca1d558b33520feba456b85f3a3cb633e46b0b7
Sayantam Dey <sayantam@gmail.com>
2018-11-21 09:52:24 +0530

    Execute command as step

--- install_all.sh
+++ install_all.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-
 # Installs dependencies for all modules
 
-pip install --upgrade pip
-npm install npm@latest -g
+pip install --upgrade pip || exit $?
+
+npm install npm@latest -g || exit $?
 
 cd hip-edit-infra
-pip install -r requirements.txt
+pip install -r requirements.txt || exit $?
 
 cd ../hip-edit-server
-npm install
+npm install || exit $?
 
 cd ../hip-edit-web
-npm install
+npm install || exit $?
+
+exit 0
