2cc83038fdc96f21c5f060c57bea835b0b132c37
Sayantam Dey <sayantam@gmail.com>
2018-11-23 10:40:44 +0530

    Download SSH private key

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 # Deploys all module dependencies
+key_dir_path="~/.ssh"
+key_name="BouncyBotKeyPair"
+key_file="${key_name}.pem"
+
+mkdir -p $key_dir_path || exit $?
+chmod 700 $key_dir_path || exit $?
+aws s3 cp s3://3pillar-eng-tools-secrets/${key_file} ${key_dir_path}/${key_file}
+chmod 400 ${key_dir_path}/${key_file}
 
 cd hip-edit-server
 npm run build-lambda || exit $?
@@ -8,7 +16,7 @@ npm run build-lambda || exit $?
 cd ../hip-edit-infra
 
 python services.py --name EngTools -s 3pillar-eng -u $CF_ROLE_ARN $* \
---vpc-id $SERVICES_VPC_ID --subnet-id $SERVICES_SUBNET_ID BouncyBotKeyPair \
+--vpc-id $SERVICES_VPC_ID --subnet-id $SERVICES_SUBNET_ID ${key_name} \
 --amq-users publishers:$PUBLISHER_USER guests:$CONSUMER_USER || exit $?
 
 npm_config_messaging_user=$PUBLISHER_USER python sam.py --name EngTools -u $CF_ROLE_ARN $* \
